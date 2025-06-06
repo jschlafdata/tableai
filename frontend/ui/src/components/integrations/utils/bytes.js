18a8620d63b178a5435d7b7fa68228584167243b
john schlafly <jschlafly@life360.com>
2025-06-06 09:41:53 -0600

    nuke

--- frontend/ui/src/components/integrations/utils/bytes.js
+++
@@ -1,6 +0,0 @@
-export default function formatBytes(bytes = 0) {
-    if (bytes < 1024) return `${bytes} B`
-    const k = 1024, dm = 2, sizes = ['KB','MB','GB','TB']
-    const i = Math.floor(Math.log(bytes) / Math.log(k))
-    return `${(bytes / Math.pow(k, i)).toFixed(dm)} ${sizes[i]}`
-  }
