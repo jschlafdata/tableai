561c63876595cde556facb990906381f5939392d
john schlafly <jschlafly@life360.com>
2025-09-02 01:47:50 -0600

    library rebuild

--- tableai_slim/tableai/__init__.py
+++ tableai_slim/tableai/__init__.py
@@ -1,8 +1,9 @@
 from importlib.metadata import version as _metadata_version
-
+from .pdf import PDFDocument
 
 __all__ = (
-    '__version__'
+    '__version__',
+    'PDFDocument'
 )
 
 __version__ = _metadata_version('tableai_slim')
